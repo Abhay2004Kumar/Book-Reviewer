3607fd3c9850e3fdb498b59ca6924d80b92c9128
Abhay2004Kumar <abs220502@gmail.com>
2025-06-18 17:29:09 +0530

    updated server.js again

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from 'cors';
 import connectDB from "./config/db.js";
 
 import userRoutes from "./routes/userRoutes.js";
@@ -12,6 +13,14 @@ dotenv.config();
 connectDB();
 
 const app = express();
+
+app.use(
+    cors({
+      origin: process.env.FRONTEND_URL || "*", // Allow all or restrict to your frontend
+      credentials: true,
+    })
+  );
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: process.env.REACT_APP_BASE_URL || "http://localhost:5000/api", // adjust if different in production
+  baseURL: process.env.REACT_APP_BASE_URL || "", // adjust if different in production
 });
 
 export default instance;
